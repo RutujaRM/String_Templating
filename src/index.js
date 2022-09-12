cc6106c55bb60bcfc593ae7b7abe900688cd80f9
Rutuja R Mashere <96306705+RutujaRM@users.noreply.github.com>
2022-09-12 00:06:42 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+// document.getElementById("app").innerHTML = `
+// <h1>Hello Vanilla!</h1>
+// <div>
+//   We use the same configuration as Parcel to bundle this sandbox, you can find more
+//   info about Parcel
+//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+// </div>
+// `;
+
+// function myfunction() {
+//   const fname = document.getElementById("fname").value;
+//   const lname = document.getElementById("lname").value;
+//   const email = document.getElementById("email").value;
+
+//   alert(
+//     `Hello ${fname} ${lname}, we will reach out to you shortly a ${email}  ...`
+//   );
+// }
+// myfunction();
